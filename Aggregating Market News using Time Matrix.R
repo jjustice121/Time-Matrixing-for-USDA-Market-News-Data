801ec2aaadb7f2f99759f739f7f86623fefbd856
JJUSTICE <52771749+jjustice121@users.noreply.github.com>
2025-02-07 10:44:59 -0500

    Add files via upload

--- Aggregating Market News using Time Matrix.R
+++ Aggregating Market News using Time Matrix.R
@@ -6,22 +6,87 @@ library(lubridate)
 library(tidyverse)
 library(USDAMARS)
 
+#set working directory
+setwd("C:/Users/jessie.justice/OneDrive - USDA/Desktop/Data Analysis Projects/EAB Data Management/Test Scripts and Programs/Test Outputs")
+
 #Set API Key
-MARS_API_Key()
+MARS_API_Key("d8qcrgndThTeo/iTpu5AJF8rqbe+AT7mImzqT4gWe2U=")
 
-#Import Time Matrix
-Time_Matrix <- read.csv()
+#write function to convert $/MT to $/LB
+Conversion_to_Dollars_Per_Lb <- function(i){
+  
+  DollarsPerLb <- i * 0.00045359237
+}
 
-Dir <- MARS_Table_Directory()
-#Get Table
-Butter <- MARS_Table_Pull(1099)
+#write function to iterate over all International DMN Reports
+Aggregate_Biweekly_to_Monthly <- function(slug_id){
+  
+  #Get Table
+  df <- MARS_Table_Pull(slug_id)
+  
+  #import time matrix
+  time_matrix <- read.xlsx(paste0(getwd(),"/International_Market_News_Time_Matrix.xlsx"), sheet = df$report_title[1])
 
-Butter <- Butter %>% select(c("report_begin_date","report_end_date","report_title","price_Unit","price_min","price_max"))
+  df <- df %>% select(c("report_begin_date","report_title","price_Unit","price_min","price_max")) %>% mutate(price_midpoint = (price_min + price_max)/2)
 
-#add unique ID
-Butter$ID <- as.numeric(factor(Butter$report_begin_date, levels = unique(Butter$report_begin_date)))
+  #add unique ID
+  df$ID <- as.numeric(factor(df$report_begin_date, levels = unique(df$report_begin_date)))
+  
+  #drop begin date for merge
+  df <- df %>% select(-c("report_begin_date"))
+  
+  #Merge Time Matrix and Data
+  df_Matrix <- df %>% left_join(time_matrix, by =c("ID"))
 
-#Merge Time Matrix and Data
-Butter_Matrix <- Butter %>% left_join(Time_Matrix, by =c("ID","report_begin_date"))
+  #list of columns to transform
+  Months <- c("January","February","March","April","May","June","July","August","September","October","November","December")
 
-#Multiply Across Rows
+  #get rowsums
+  df_Matrix$Days_Total <- rowSums(df_Matrix[,Months], na.rm = TRUE)
+
+  #get total business days by month-year combination
+  df_Matrix <- df_Matrix %>% group_by(`Month-Year`) %>% mutate(Days_Total_Month = sum(Days_Total)) %>% ungroup()
+
+  #multiply price columns by Days_total
+  df_Matrix <- df_Matrix %>% mutate(price_min_prod_days = price_min * Days_Total, price_max_prod_days = price_max * Days_Total, price_midpoint_prod_days = price_midpoint * Days_Total)
+
+  #get monthly aggregate products
+  df_Matrix <- df_Matrix %>% group_by(`Month-Year`) %>% mutate(price_min_prod_days_agg = sum(price_min_prod_days), price_max_prod_days_agg = sum(price_max_prod_days), price_midpoint_prod_days_agg = sum(price_midpoint_prod_days)) %>% ungroup()
+
+  #divide by total number of business days in month
+  df_Matrix <- df_Matrix %>% mutate(price_min_monthly = price_min_prod_days_agg/Days_Total_Month, price_max_monthly = price_max_prod_days_agg/Days_Total_Month, price_midpoint_monthly = price_midpoint_prod_days_agg/Days_Total_Month)
+
+  #convert to dollar per lb
+
+  #function to convert from $/mt to $/lb
+  df_Matrix <- df_Matrix %>% mutate_at(c("price_min_monthly","price_max_monthly","price_midpoint_monthly"),Conversion_to_Dollars_Per_Lb)
+
+  #drop columns we don't need
+  df_Matrix <- df_Matrix %>% select(-c(price_Unit,price_min_prod_days,price_max_prod_days,price_midpoint_prod_days,price_min_prod_days_agg,price_max_prod_days_agg,price_midpoint_prod_days_agg))
+
+  #aggregate dataframe to get a single value for each month-year combination
+  df_Matrix <- df_Matrix %>% select(-c(Months,Days_Total, price_min,price_max,price_midpoint,ID,report_begin_date)) %>% distinct()
+
+  assign(df$report_title[1],df_Matrix, envir = .GlobalEnv)
+  
+  
+}
+
+
+#test function
+#Aggregate_Biweekly_to_Monthly(1034)
+
+#European Whey, Oceania SMP, Oceania Cheese, Oceania Butter
+Report_List <- c(1034,1038,1082,1099)
+
+#Iterate Over List
+lapply(Report_List,Aggregate_Biweekly_to_Monthly)
+
+#export to excel file
+
+
+#create list of sheets for Excel File
+sheets <- list("Butter - Oceania" = `Butter - Oceania`, "Cheese - Oceania" = `Cheese - Oceania`,"Skim Milk Powder - Oceania" = `Skim Milk Powder - Oceania`,"Dry Whey - Europe" = `Dry Whey - Europe`)
+
+#Write Sheets to Excel File
+write.xlsx(sheets,"C:/Users/jessie.justice/OneDrive - USDA/Desktop/Data Analysis Projects/EAB Data Management/Test Scripts and Programs/Test Outputs/International_Monthly_Market_News_Data.xlsx")
--- Time Matrixing Script.R
+++ Time Matrixing Script.R
@@ -4,66 +4,113 @@
 library(lubridate)
 library(tidyverse)
 library(USDAMARS)
+library(openxlsx)
 
 #Set API Key
-MARS_API_Key()
+MARS_API_Key("d8qcrgndThTeo/iTpu5AJF8rqbe+AT7mImzqT4gWe2U=")
 
+#get MARS report directory
 Dir <- MARS_Table_Directory()
-#Get Table
-Butter <- MARS_Table_Pull(1099)
 
-Butter <- Butter %>% select(c("report_begin_date","report_end_date"))
+#Write function for create time matrix
 
-#get days in range
+Time_Matrixing <- function(slug_id){
+  
+  df <- MARS_Table_Pull(slug_id)
+  
+  title <- df$report_title[1]
 
-#add unique ID
-Butter$ID <- as.numeric(factor(Butter$report_begin_date, levels = unique(Butter$report_begin_date)))
+  df <- df %>% select(c("report_begin_date","report_end_date"))
+  
+  df <- df %>% filter(!(report_begin_date == report_end_date))
 
-#functions to get day count in each respective month within range for report
+  #get days in range
 
-f1 <- function(d_first,d_last){
-  d_first <- mdy(d_first)
-  d_last <- mdy(d_last)
+  #add unique ID
+  df$ID <- as.numeric(factor(df$report_begin_date, levels = unique(df$report_begin_date)))
+
+  #functions to get day count in each respective month within range for report
+
+  f1 <- function(d_first,d_last){
+    d_first <- mdy(d_first)
+    d_last <- mdy(d_last)
   
   
-  D <- seq.Date(d_first, d_last, 1) # generate all days in [d_first,d_last]
+    D <- seq.Date(d_first, d_last, 1) # generate all days in [d_first,d_last]
   
-  D <- D[!weekdays(D) %in% c("Saturday","Sunday")]
+    D <- D[!weekdays(D) %in% c("Saturday","Sunday")]
+    
+    if(length(D) < 10){
+      
+      D <- seq.Date(d_first,d_last + 4,1)
+      
+    }else{
+      
+      D <- seq.Date(d_first,d_last,1)
+    }
+    
+    D <- D[!weekdays(as.Date(D)) %in% c("Saturday","Sunday")]
+    
+    print(D)
+    
+    M <- unique(format(D, "%m")) # all months in [d_first,d_lst]
   
-  M <- unique(format(D, "%m")) # all months in [d_first,d_lst]
   
+    f2 <- function(x) length(which(format(D, "%m") == x)) # returns number of days in month x
   
-  f2 <- function(x) length(which(format(D, "%m") == x)) # returns number of days in month x
+    res <- vapply(M,f2,numeric(1))
   
-  res <- vapply(M,f2,numeric(1))
+    return(cbind(unique(format(D, "%Y-%m")),res))
+  }
+
+  f3 <- function(k) f1(df$report_begin_date[k],df$report_end_date[k])
+
+  f4 <- function(k) cbind(df$ID[k],df$report_begin_date[k], f3(k))
+
+
+  Days_output <- sapply(1:nrow(df), f4)
+
+  Days_df <- as.data.frame(do.call(rbind, Days_output))
+
+  colnames(Days_df) <- c("ID","report_begin_date","Month-Year","Number_of_Business_Days") 
+
+  Days_df$Month <- substr(Days_df$`Month-Year`,6,7)
+
+  Days_df <- Days_df %>% pivot_wider(names_from = Month,values_from = Number_of_Business_Days) %>% mutate(ID = as.numeric(ID))
+
+  Days_df <- Days_df %>% select(c("ID","report_begin_date","Month-Year","01","02","03","04","05","06","07","08","09","10","11","12")) 
+
+  colnames(Days_df)[4:15] <- month.name
+
+  #merge Days_df into the original Butter df on ID and report begin date
+
+  Matrix <- Days_df %>% left_join(df, by = c("ID","report_begin_date")) %>% select(-c("report_end_date"))
   
-  return(cbind(unique(format(D, "%Y-%m")),res))
+  #vector of month names
+  Months <- c("January","February","March","April","May","June","July","August","September","October","November","December")
+  
+  #transform columns
+  Matrix[,Months] <- lapply(Matrix[,Months], function(x) substr(x,1,2))
+  
+  #transform to numeric
+  Matrix[,Months] <- lapply(Matrix[,Months], function(x) as.numeric(x))
+  
+  assign(title,Matrix, envir = .GlobalEnv)
+  
 }
 
-f3 <- function(k) f1(Butter$report_begin_date[k],Butter$report_end_date[k])
+#Iterate Over List of Reports
 
-f4 <- function(k) cbind(Butter$ID[k],Butter$report_begin_date[k], f3(k))
+#report list
+Report_List <- c(1034,1038,1082,1099)
 
-
-Days_output <- sapply(1:nrow(Butter), f4)
-
-Days_df <- as.data.frame(do.call(rbind, Days_output))
-
-colnames(Days_df) <- c("ID","report_begin_date","Month-Year","Number_of_Business_Days") 
-
-Days_df$Month <- substr(Days_df$`Month-Year`,6,7)
-
-Days_df <- Days_df %>% pivot_wider(names_from = Month,values_from = Number_of_Business_Days) %>% mutate(ID = as.numeric(ID))
-
-Days_df <- Days_df %>% select(c("ID","report_begin_date","Month-Year","01","02","03","04","05","06","07","08","09","10","11","12")) 
-
-colnames(Days_df)[4:15] <- month.name
-
-#merge Days_df into the original Butter df on ID and report begin date
-
-International_Time_Matrix_MMN <- Days_df %>% left_join(Butter, by = c("ID","report_begin_date")) %>% relocate(report_end_date,.after = report_begin_date)
-
+lapply(Report_List,Time_Matrixing)
 
 #export dataframe
-write.csv(International_Time_Matrix_MMN, row.names = FALSE)
+
+#create list of sheets for Excel File
+sheets <- list("Butter - Oceania" = `Butter - Oceania`, "Cheese - Oceania" = `Cheese - Oceania`,"Skim Milk Powder - Oceania" = `Skim Milk Powder - Oceania`,"Dry Whey - Europe" = `Dry Whey - Europe`)
+
+#Write Sheets to Excel File
+write.xlsx(sheets,"C:/Users/jessie.justice/OneDrive - USDA/Desktop/Data Analysis Projects/EAB Data Management/Test Scripts and Programs/Test Outputs/International_Market_News_Time_Matrix.xlsx")
 
